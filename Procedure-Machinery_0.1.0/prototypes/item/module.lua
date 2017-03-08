13a2de2dde01d11902c6431b71a2e3ddb30da696
Huawei Wang <huawei_007@yahoo.com>
2017-03-08 18:22:15 +0800

    buffs effectivity modules

--- Procedure-Machinery_0.1.0/prototypes/item/module.lua
+++ Procedure-Machinery_0.1.0/prototypes/item/module.lua
@@ -3,84 +3,84 @@ require("config")
 
 -- I always have the safety measures in case users mess up the config.
 if module_maxlevel <= 3 then
-    module_maxlevel = 4
+  module_maxlevel = 4
 elseif module_maxlevel > 25 then
-    module_maxlevel = 25
+  module_maxlevel = 25
 end
 
 -- adding the modules based on config. check config for details.
+module_data = {}
 for i  = 4, module_maxlevel do
-    data:extend(
+  table.insert(module_data,
     {
-        {
-            type = "module",
-            name = "speed-module-" .. i,
-            icon = "__Procedure-Machinery__/graphics/icons/module/speed-module-" .. i ..".png",
-            flags = {"goes-to-main-inventory"},
-            subgroup = "module",
-            category = "speed",
-            tier = i,
-            order = "a[speed]-d[speed-module-" .. i .. "]",
-            stack_size = 50,
-            default_request_amount = 10,
-            effect = { speed = {bonus = 0.2 + i ^ module_power * 0.1}, consumption = {bonus = 0.4 + i ^ module_power * 0.1}}
-        },
-        {
-            type = "module",
-            name = "effectivity-module-" .. i,
-            icon = "__Procedure-Machinery__/graphics/icons/module/effectivity-module-" .. i ..".png",
-            flags = {"goes-to-main-inventory"},
-            subgroup = "module",
-            category = "effectivity",
-            tier = i,
-            order = "c[effectivity]-d[effectivity-module-" .. i .. "]",
-            stack_size = 50,
-            default_request_amount = 10,
-            effect = { consumption = {bonus = -0.2 - i ^ module_power / 10}},
-            limitation = production
-        },
-        {
-            type = "module",
-            name = "productivity-module-" .. i,
-            icon = "__Procedure-Machinery__/graphics/icons/module/productivity-module-" .. i ..".png",
-            flags = {"goes-to-main-inventory"},
-            subgroup = "module",
-            category = "productivity",
-            tier = i,
-            order = "c[productivity]-d[productivity-module-" .. i .. "]",
-            stack_size = 50,
-            default_request_amount = 10,
-            effect = { productivity = {bonus = 0.025 + i ^ module_power * 0.025}, consumption = {bonus = 0.2 + i ^ module_power / 5}, speed = {bonus = -0.15}},
-            limitation = productivitymodulelimitation(),
-            limitation_message_key = "production-module-usable-only-on-intermediates"
-        },
-    }
-    )
+      type = "module",
+      name = "speed-module-" .. i,
+      icon = "__Procedure-Machinery__/graphics/icons/module/speed-module-" .. i ..".png",
+      flags = {"goes-to-main-inventory"},
+      subgroup = "module",
+      category = "speed",
+      tier = i,
+      order = "a[speed]-d[speed-module-" .. i .. "]",
+      stack_size = 50,
+      default_request_amount = 10,
+      effect = { speed = {bonus = 0.2 + i ^ module_power * 0.1}, consumption = {bonus = 0.4 + i ^ module_power * 0.1}}
+    },
+    {
+      type = "module",
+      name = "effectivity-module-" .. i,
+      icon = "__Procedure-Machinery__/graphics/icons/module/effectivity-module-" .. i ..".png",
+      flags = {"goes-to-main-inventory"},
+      subgroup = "module",
+      category = "effectivity",
+      tier = i,
+      order = "c[effectivity]-d[effectivity-module-" .. i .. "]",
+      stack_size = 50,
+      default_request_amount = 10,
+      effect = { consumption = {bonus = i ^ module_power / -2.5}},
+      limitation = production
+    },
+    {
+      type = "module",
+      name = "productivity-module-" .. i,
+      icon = "__Procedure-Machinery__/graphics/icons/module/productivity-module-" .. i ..".png",
+      flags = {"goes-to-main-inventory"},
+      subgroup = "module",
+      category = "productivity",
+      tier = i,
+      order = "c[productivity]-d[productivity-module-" .. i .. "]",
+      stack_size = 50,
+      default_request_amount = 10,
+      effect = { productivity = {bonus = 0.025 + i ^ module_power * 0.025}, consumption = {bonus = 0.2 + i ^ module_power / 5}, speed = {bonus = -0.15}},
+      limitation = productivitymodulelimitation(),
+      limitation_message_key = "production-module-usable-only-on-intermediates"
+    },
+  )
 end
+data:extend(module_data)
 
 -- The design is that productivity modules are usable only in parts, but provides better productivity bonus and no extra pollution, only pollution from extra energy use.
 -- Another module called industrial module can increase productivity in anything, but causes heavy pollution as well as extreme power usage (no speed penalty though).
 --[[for i  = 1, module_maxlevel do
-    data:extend(
+  data:extend(
+  {
     {
-        {
-            type = "module",
-            name = "industrial-module-" .. i,
-            icon = "__Procedure-Machinery__/graphics/icons/module/industrial-module-" .. i ..".png",
-            flags = {"goes-to-main-inventory"},
-            subgroup = "module",
-            category = "industrial",
-            tier = i,
-            order = "a[industrial]-a[industrial-module-" .. i .. "]",
-            stack_size = 50,
-            default_request_amount = 10,
-            effect = { productivity = {bonus = i ^ module_power * 0.015}, consumption = {bonus = i ^ module_power / 2}, pollution = {bonus = 0.2 * i ^ module_power}},
-        }
+      type = "module",
+      name = "industrial-module-" .. i,
+      icon = "__Procedure-Machinery__/graphics/icons/module/industrial-module-" .. i ..".png",
+      flags = {"goes-to-main-inventory"},
+      subgroup = "module",
+      category = "industrial",
+      tier = i,
+      order = "a[industrial]-a[industrial-module-" .. i .. "]",
+      stack_size = 50,
+      default_request_amount = 10,
+      effect = { productivity = {bonus = i ^ module_power * 0.015}, consumption = {bonus = i ^ module_power / 2}, pollution = {bonus = 0.2 * i ^ module_power}},
     }
-    )
+  }
+  )
 end]]--
 
--- modifying the base productivity modules to remove the pollution penalty.
+-- modifying the base productivity modules to remove the pollution penalty, also buffed the effectivity modules.
 data:extend(
 {
   {
@@ -127,6 +127,46 @@ data:extend(
     effect = { productivity = {bonus = 0.1}, consumption = {bonus = 0.8}, speed = {bonus = -0.15}},
     limitation = productivitymodulelimitation(),
     limitation_message_key = "production-module-usable-only-on-intermediates"
+  },
+  {
+    type = "module",
+    name = "effectivity-module",
+    icon = "__base__/graphics/icons/effectivity-module.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "module",
+    category = "effectivity",
+    tier = 1,
+    order = "c[effectivity]-a[effectivity-module-1]",
+    stack_size = 50,
+    default_request_amount = 10,
+    effect = { consumption = {bonus = -0.4}},
+    limitation = production
+  },
+  {
+    type = "module",
+    name = "effectivity-module-2",
+    icon = "__base__/graphics/icons/effectivity-module-2.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "module",
+    category = "effectivity",
+    tier = 2,
+    order = "c[effectivity]-b[effectivity-module-2]",
+    stack_size = 50,
+    default_request_amount = 10,
+    effect = { consumption = {bonus = -0.8}}
+  },
+  {
+    type = "module",
+    name = "effectivity-module-3",
+    icon = "__base__/graphics/icons/effectivity-module-3.png",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "module",
+    category = "effectivity",
+    tier = 3,
+    order = "c[effectivity]-c[effectivity-module-3]",
+    stack_size = 50,
+    default_request_amount = 10,
+    effect = { consumption = {bonus = -1.2}}
   }
 }
 )
--- Procedure-Machinery_0.1.0/prototypes/recipe/module.lua
+++ Procedure-Machinery_0.1.0/prototypes/recipe/module.lua
@@ -3,56 +3,56 @@ require("config")
 
 -- I always have the safety measures in case users mess up the config.
 if module_maxlevel <= 3 then
-    module_maxlevel = 4
+  module_maxlevel = 4
 elseif module_maxlevel > 25 then
-    module_maxlevel = 25
+  module_maxlevel = 25
 end
 
 for i  = 4, module_maxlevel do
-    data:extend(
+  data:extend(
+  {
     {
-        {
-            type = "recipe",
-            name = "speed-module-" .. i,
-            enabled = true,
-            ingredients =
-            {
-                {"speed-module-" .. (i - 1), 4},
-                {"advanced-circuit", 5 * (i - 2)},
-                {"processing-unit", 5 * (i - 2)},
-                {"alien-artifact", 1 * (i - 2)}
-            },
-            energy_required = 15 * 2 ^ (i - 1),
-            result = "speed-module-" .. i
-        },
-        {
-            type = "recipe",
-            name = "effectivity-module-" .. i,
-            enabled = true,
-            ingredients =
-            {
-                {"effectivity-module-" .. (i - 1), 4},
-                {"advanced-circuit", 5 * (i - 2)},
-                {"processing-unit", 5 * (i - 2)},
-                {"alien-artifact", 1 * (i - 2)}
-            },
-            energy_required = 15 * 2 ^ (i - 1),
-            result = "effectivity-module-" .. i
-        },
-        {
-            type = "recipe",
-            name = "productivity-module-" .. i,
-            enabled = true,
-            ingredients =
-            {
-                {"productivity-module-" .. (i - 1), 4},
-                {"advanced-circuit", 5 * (i - 2)},
-                {"processing-unit", 5 * (i - 2)},
-                {"alien-artifact", 1 * (i - 2)}
-            },
-            energy_required = 15 * 2 ^ (i - 1),
-            result = "productivity-module-" .. i
-        },
-    }
-    )
+      type = "recipe",
+      name = "speed-module-" .. i,
+      enabled = false,
+      ingredients =
+      {
+        {"speed-module-" .. (i - 1), 4},
+        {"advanced-circuit", 5 * (i - 2)},
+        {"processing-unit", 5 * (i - 2)},
+        {"alien-artifact", 1 * (i - 2)}
+      },
+      energy_required = 15 * 2 ^ (i - 1),
+      result = "speed-module-" .. i
+    },
+    {
+      type = "recipe",
+      name = "effectivity-module-" .. i,
+      enabled = false,
+      ingredients =
+      {
+        {"effectivity-module-" .. (i - 1), 4},
+        {"advanced-circuit", 5 * (i - 2)},
+        {"processing-unit", 5 * (i - 2)},
+        {"alien-artifact", 1 * (i - 2)}
+      },
+      energy_required = 15 * 2 ^ (i - 1),
+      result = "effectivity-module-" .. i
+    },
+    {
+      type = "recipe",
+      name = "productivity-module-" .. i,
+      enabled = false,
+      ingredients =
+      {
+        {"productivity-module-" .. (i - 1), 4},
+        {"advanced-circuit", 5 * (i - 2)},
+        {"processing-unit", 5 * (i - 2)},
+        {"alien-artifact", 1 * (i - 2)}
+      },
+      energy_required = 15 * 2 ^ (i - 1),
+      result = "productivity-module-" .. i
+    },
+  }
+  )
 end
