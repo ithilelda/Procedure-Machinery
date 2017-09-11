73f9331dbf2bc61e19159a8c210b7f94017771a0
Huawei Wang <huawei_007@yahoo.com>
2017-09-11 20:38:24 +0800

    add modification to the vanilla robot researches. This shouldn't be in this mod but I will separate them in the future.

--- Procedure-Machinery_0.3.0/common.lua
+++ Procedure-Machinery_0.3.0/common.lua
@@ -7,18 +7,31 @@ function order_string (i)
 end
 
 function tech_add_unlock (technology_name, recipe_name)
-  if data.raw.technology[technology_name] then
+  if data.raw.technology[technology_name] and data.raw.technology[technology_name].effects then
     local recipe = {type = "unlock-recipe", recipe = recipe_name}
     table.insert(data.raw.technology[technology_name].effects, recipe)
   end
 end
 
 function tech_remove_unlock (technology_name, recipe_name)
-  if data.raw.technology[technology_name] then
+  if data.raw.technology[technology_name] and data.raw.technology[technology_name].effects then
     effects = data.raw.technology[technology_name].effects
     for i, t in ipairs(effects) do
-      if t.recipe == recipe_name then
+      if t.type == "unlock-recipe" and t.recipe == recipe_name then
         table.remove(data.raw.technology[technology_name].effects, i)
+        break
+      end
+    end
+  end
+end
+
+function tech_remove_pack (technology_name, pack_name)
+  if data.raw.technology[technology_name] and data.raw.technology[technology_name].unit and data.raw.technology[technology_name].unit.ingredients then
+    ingredients = data.raw.technology[technology_name].unit.ingredients
+    for i, t in ipairs(ingredients) do
+      if t[1] == pack_name then
+        table.remove(data.raw.technology[technology_name].unit.ingredients, i)
+        break
       end
     end
   end
--- Procedure-Machinery_0.3.0/prototypes/technology/crafting.lua
+++ Procedure-Machinery_0.3.0/prototypes/technology/crafting.lua
@@ -13,3 +13,12 @@ tech_remove_unlock("advanced-oil-processing", "heavy-oil-cracking")
 tech_remove_unlock("advanced-oil-processing", "light-oil-cracking")
 tech_add_unlock("advanced-oil-processing", "heavy-oil-cracking-catalyst")
 tech_add_unlock("advanced-oil-processing", "light-oil-cracking-catalyst")
+
+-- rework some of the robotics. logistic robot tech now unlocks requester chests and active providers but not storage nor passive providers.
+tech_remove_unlock("logistic-robotics", "logistic-chest-passive-provider")
+tech_remove_unlock("logistic-robotics", "logistic-chest-storage")
+tech_add_unlock("logistic-robotics", "logistic-chest-active-provider")
+tech_add_unlock("logistic-robotics", "logistic-chest-requester")
+
+-- now you can research bot speed 1 without blue packs.
+tech_remove_pack("worker-robots-speed-1", "science-pack-3")
